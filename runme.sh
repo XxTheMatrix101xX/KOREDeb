91fdd9cad55f4c894fb95b85cd37e2e6aab6ba22
TheMatrix101 <33000692+XxTheMatrix101xX@users.noreply.github.com>
2018-04-13 10:54:43 -0500

    updated runme.sh
    added libevent-dev which was missing, now a completely clean system can run runme.sh and have KORE running in seconds.

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo|sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
-sudo apt install -y autotools-dev automake libtool libprotobuf-dev protobuf-compiler build-essential libdb4.8-dev libdb4.8++-dev libboost-all-dev pkg-config libcurl4-openssl-dev
+sudo apt install -y autotools-dev automake libtool libprotobuf-dev protobuf-compiler build-essential libdb4.8-dev libdb4.8++-dev libboost-all-dev pkg-config libcurl4-openssl-dev libevent-dev
 sudo dpkg -i kore_0.12.5.0-1_x64.deb
 clear
 echo Please start your updated KORE daemon with the following command "kored --daemon" if you are using headless
